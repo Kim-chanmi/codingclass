82adc38f1d39e321ba1cc340d39164d1fbdd1ead
Kim-chanmi <chanmi1320@gmail.com>
2022-10-31 09:24:47 +0900

    2022.10.31

--- javascript/assets/js/memory.js
+++ javascript/assets/js/memory.js
@@ -3,11 +3,17 @@
 // 03. 두개의 카드 뒤집어서 확인(첫 번째, 두 번째)
 
 const memoryWrap = document.querySelector(".memory__wrap");
+const memoryCard = document.querySelector(".memory__card");
 const memoryCards = memoryWrap.querySelectorAll(".cards li");
+const memoryStart = document.querySelector(".memory__start");
+const memoryIntro = document.querySelector(".memory__intro");
+const memoryRestart = document.querySelector(".memory__restart");
+const memoryRestart2 = document.querySelector(".memory__restart2");
 
 let cardOne, cardTwo;
 let disableDeck = false;
 let matchedCard = 0;
+let oppunity = 3; // 기회
 
 let sound = [
   "../assets/audio/right.m4a",
@@ -44,8 +50,10 @@ function matchCards(img1, img2) {
   if (img1 == img2) {
     // 같을 경우
     matchedCard++;
+    document.querySelector(".memory__score").innerHTML = matchedCard;
     if (matchedCard == 8) {
       soundSuccess.play();
+      memoryRestart2.style.transform = "scale(1)";
     }
     cardOne.removeEventListener("click", flipCard);
     cardTwo.removeEventListener("click", flipCard);
@@ -66,8 +74,22 @@ function matchCards(img1, img2) {
       cardOne = cardTwo = "";
       disableDeck = false;
     }, 1600);
-
     soundUnMatch.play();
+
+    oppunity--;
+    document.querySelector(".memory__opportuvity").textContent = oppunity;
+    if (oppunity == 0) {
+      setTimeout(() => {
+        memoryCards.forEach((card) => {
+          card.classList.add("flip");
+          card.removeEventListener("click", flipCard);
+        });
+      }, 1000);
+      setTimeout(() => {
+        document.querySelector(".memory__restart").style.transform = "scale(1)";
+        memoryCards.style.transform = "scale(0)";
+      }, 2000);
+    }
   }
 }
 
@@ -106,15 +128,50 @@ function shuffledCard() {
 
     setTimeout(() => {
       card.classList.remove("flip");
-    }, 6000);
+    }, 5000);
 
     let imgTag = card.querySelector(".back img");
     imgTag.src = `../assets/img/cardGame${arr[index]}.jpg`;
   });
 }
-shuffledCard();
 
 // 카드 클릭
 memoryCards.forEach((card) => {
   card.addEventListener("click", flipCard);
+});
+
+// 카드 게임 시작
+memoryStart.addEventListener("click", () => {
+  memoryIntro.style.display = "none";
+  memoryCard.style.display = "block";
+  shuffledCard();
+  matchedCard = 0;
+});
+
+// 다 맞추고 다시 시작
+memoryRestart2.addEventListener("click", () => {
+  shuffledCard();
+  oppunity = 3;
+  document.querySelector(".memory__opportuvity").innerHTML = oppunity;
+  matchedCard = 0;
+  document.querySelector(".memory__score").innerHTML = matchedCard;
+  memoryCards.forEach((card) => {
+    card.classList.add("flip");
+    card.removeEventListener("click", flipCard);
+  });
+  memoryRestart2.style.transform = "scale(0)";
+});
+
+// 실패하고 다시 시작
+memoryRestart.addEventListener("click", () => {
+  shuffledCard();
+  oppunity = 3;
+  document.querySelector(".memory__opportuvity").innerHTML = oppunity;
+  matchedCard = 0;
+  document.querySelector(".memory__score").innerHTML = matchedCard;
+  memoryCards.forEach((card) => {
+    card.classList.add("flip");
+    card.removeEventListener("click", flipCard);
+  });
+  memoryRestart.style.transform = "scale(0)";
 });
