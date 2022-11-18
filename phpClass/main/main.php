3bcaab249f08d5375374cfa2fc1839a4c050d1ab
Kim-chanmi <chanmi1320@gmail.com>
2022-11-18 17:21:48 +0900

    2022.11.18

--- phpClass/main/main.php
+++ phpClass/main/main.php
@@ -26,7 +26,20 @@
     <!-- header -->
 
     <main id="main">
-
+        <section id="banner">
+            <h2>블로그 소개입니다.</h2>
+            <div class="banner__inner container">
+                <div class="img">
+                    <img src="../assets/img/banner_bg01.jpg" alt="배너 이미지">
+                </div>
+                <div class="desc">
+                    어떤 일이라도 <em>노력</em>하고 즐기면 그 결과는 <em>빛</em>을 발한다고 생각합니다.
+                    신입의 <em>열정</em>과 <em>도전정신</em>을 깊숙히 새기며 <em>배움</em>에 있어 겸손함을
+                    유지하며 세부적인 곳까지 파고드는 <em>개발자</em>가 되겠습니다.
+                </div>
+            </div>
+        </section>
+        <!-- banner -->
     </main>
     <!-- main -->
 
